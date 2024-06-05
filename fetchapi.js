6fe930a03486a0183effa1dac21ed9edb9304f7d
MoMoktan <112465095+MoMoktan@users.noreply.github.com>
2024-06-05 21:10:46 +0545

    Update fetchapi.js

--- fetchapi.js
+++ fetchapi.js
@@ -15,11 +15,34 @@ import fetch from "node-fetch";
 // })
 
 
-fetch("https://www.craigslist.org/about/")
-.then((response)=> {
-    return response.text()
-}).then((html)=> {
-console.log(html);
-})
+// fetch("https://www.craigslist.org/about/")
+// .then((response)=> {
+//     return response.text()
+// }).then((html)=> {
+// console.log(html);
+// })
+
+
+// const jsonfetch = fetch ("https://jsonplaceholder.typicode.com/posts/1");
+
+// jsonfetch.then((value)=> {
+//     return value.json()
+// }).then((jsonfetch)=>{
+// console.log(jsonfetch)
+// console.log(jsonfetch.id)
+// })
+
+
+const pokemon = fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
+
+pokemon.then((response) => response.json())
+  .then((pokemon) => {
+    //console.log(pokemon);
+    console.log(pokemon.sprites.other["official-artwork"].front_default);
+  })
+  .catch((error) => {
+    console.error("Error fetching Pokemon data:", error);
+  });
+
 
 
